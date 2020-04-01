af09cc98c7cd844c5d9be30779988e20cf250576
Karthikeyan Palaniswamy <karthikeyan.forethought@gmail.com>
2020-04-01 17:12:10 +0200

    Update componentAfterUpdate method with fetching update for messages.

--- src/components/Messenger/Chat/ChatContainer.js
+++ src/components/Messenger/Chat/ChatContainer.js
@@ -1,66 +1,56 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import * as api from '../../../api/message'
 import Chat from './Chat'
 
 class ChatContainer extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props){
+        super(props)
 
-    this.state = {
-      messages: [],
+        this.state = {
+            messages : [],
+        }
     }
-  }
 
-  componentDidMount() {
-    this.fetchMessages(this.props.match.params.username)
-  }
+    componentDidMount(){
+        this.fetchMessages(this.props.match.params.username)
+    }
 
-  fetchMessages = username => {
-    this.setState({
-      messages: [],
-    })
-    // the following setTimeout is to simulate network latency in order to show a "loading" component
-    setTimeout(() => {
-      api.fetchMessages(username).then(messages => {
+    fetchMessages = username=>{
         this.setState({
-          messages,
+            messages : [],
         })
-      })
-    }, 1000)
-  }
+        // the following setTimeout is to simulate network latency in order to show a "loading" component
+        setTimeout(()=>{
+            api.fetchMessages(username).then(messages=>{
+                this.setState({
+                    messages,
+                })
+            })
+        }, 1000)
+    }
 
-  // https://reactjs.org/docs/react-component.html#componentdidupdate
-  componentDidUpdate(prevProps) {
-    /* Hint. Now you don't need to iterate the messages array
-    to see if the username in the url is different from the username's conversation you
-    are displaying. Use the prevProps parameter and the this.props in the following condition to
-    replace the hardcoded false  */
-    /*const needsToFetchUser =  false
-    if (needsToFetchUser) {
-      this.fetchMessages(this.props.match.params.username)
-    }*/
-  }
-
-  render() {
-    const { messages } = this.state
-    const { match } = this.props
-
-    // QUESTION 6. Do you think this is a good place to have this needsToFetchUser logic?
-    // Can you please move needsToFetchUser condition to ChatContainer.componentDidUpdate method?
     // https://reactjs.org/docs/react-component.html#componentdidupdate
-   /* const needsToFetchUser =
-      messages.length &&
-      !messages.find(({ from, to }) => to === username || from === username)
-    if (needsToFetchUser) {
-      this.fetchMessages(this.props.match.params.username)
-    }*/
+    componentDidUpdate(prevProps){
+        /* Hint. Now you don't need to iterate the messages array
+        to see if the username in the url is different from the username's conversation you
+        are displaying. Use the prevProps parameter and the this.props in the following condition to
+        replace the hardcoded false  */
+        const needsToFetchUser = prevProps !== this.props
+        if (needsToFetchUser) {
+            this.fetchMessages(this.props.match.params.username)
+        }
+    }
 
-    return (
-        <Chat messages={messages} match={match} />
-      // hint, which component and props do you think we should return here?
-    )
-  }
+    render(){
+        const { messages } = this.state
+        const { match } = this.props
+
+        return (
+            <Chat messages={messages} match={match}/>
+            // hint, which component and props do you think we should return here?
+        )
+    }
 }
 
 export default ChatContainer
