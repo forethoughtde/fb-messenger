411c6940cb527fe8bbeb138375dd1e4d846f83cb
lkurzweg <lin.kurzweg@gmail.com>
2020-04-01 16:16:19 +0200

    Add the Route for not found pages

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ const App = () => (
       <Route path="/messages" component={Messenger} />
       <Route path="/login" component={Login} />
       <Route path="/profiles" component={Profile} />
-      <Route path="?" component={NotFound} />
+      <Route path="*" component={NotFound} />
     </Switch>
     <Footer />
   </div>
