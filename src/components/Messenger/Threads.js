ec59c4e51322e20c3b32b4c745ba7950923fa405
lkurzweg <lin.kurzweg@gmail.com>
2020-04-01 16:42:19 +0200

    Refactor Threads into a functional component

--- src/components/Messenger/Threads.js
+++ src/components/Messenger/Threads.js
@@ -1,49 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Link } from 'react-router-dom'
 
 import Avatar from '../Layout/Avatar'
 import Icon from '../Layout/Icon'
 
-class Threads extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const { threads = [] } = this.props;
-
-    return (
-      <div className="threads">
-        <div className="thread-bar">
-          <h2>
-            <Icon name="cog" />
-            Messenger
-            <Icon name="edit" />
-          </h2>
-        </div>
-        <Route
-          render={({ match, history }) => (
-            <ul className="thread-list">
-              {threads.map((thread, i) => (
-                <li
-                  key={i}
-                  onClick={() =>
-                    history.push(`${match.url}/${thread.username}`)
-                  }
-                >
-                  <Avatar username={thread.username} size="large" />
-                  <p className="user-name">
-                    <span>{`${thread.name.first} ${thread.name.last}`}</span>
-                    <small>{`${thread.lastMessage.message}`}</small>
-                  </p>
-                </li>
-              ))}
-            </ul>
-          )}
-        />
+const Threads = ({ threads }) => {
+  return (
+    <div className="threads">
+      <div className="thread-bar">
+        <h2>
+          <Icon name="cog" />
+          Messenger
+          <Icon name="edit" />
+        </h2>
       </div>
-    )
-  }
-}
+      <Route
+        render={({ match, history }) => (
+          <ul className="thread-list">
+            {threads.map((thread, i) => (
+              <li
+                key={i}
+                onClick={() =>
+                  history.push(`${match.url}/${thread.username}`)
+                }
+              >
+                <Avatar username={thread.username} size="large" />
+                <p className="user-name">
+                  <span>{`${thread.name.first} ${thread.name.last}`}</span>
+                  <small>{`${thread.lastMessage.message}`}</small>
+                </p>
+              </li>
+            ))}
+          </ul>
+        )}
+      />
+    </div>
+  )
+};
 
 export default Threads
